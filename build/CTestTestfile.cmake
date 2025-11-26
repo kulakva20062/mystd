e1ade0325fb4444edfc51c50376614f42c6bdd2d
kulakva <kulakva2006@mail.ru>
2025-11-26 10:52:02 +0300

    mystd

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/admin/Documents/CPP/mystd
-# Build directory: /home/admin/Documents/CPP/mystd/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/admin/Documents/CPP/mystd/tests
-# Build directory: /home/admin/Documents/CPP/mystd/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
